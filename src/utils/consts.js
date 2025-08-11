536379be7a89e0f35ed8d40c236d46dc1480aad1
rigonroitiv <rigon.roitiv@gmail.com>
2025-08-11 16:22:03 +0200

    m

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,6 +1,6 @@
 export const mainUrl = 'https://aura-residence.roitiv.com/projektet';
-export const BASE_URL = 'http://localhost:8080';
-// export const BASE_URL = 'https://srv412786.hstgr.cloud:7151'
+// export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'https://srv412786.hstgr.cloud:7151'
 export const objectImageUrl = 'images/'
 export const planmetricImageUrl = '/images/planimetrit/'
 // export const BASE_URL = 'http://localhost:8845';
